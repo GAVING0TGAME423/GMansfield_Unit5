78fb1a70dca5197f64829ed2fd12000db3cfb6e0
GAVING0TGAME423 <78512752+GAVING0TGAME423@users.noreply.github.com>
2021-05-06 14:06:42 -0700

    Lesson A

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> prefab;
+    private const float spawnrate = 1.5f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+   IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnrate);
+            Instantiate(prefab[Random.Range(0, prefab.Count)]);
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    private const float minforce = 10;
+    private const float maxforce = 15;
+    private const float minTorque = -10;
+    private const float maxTorque = 10;
+    private const float minxpos = -3;
+    private const float maxxpos = 3;
+    private const float yspawnpos = -2;
+
+
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        RandomForce();
+        RandomTorque();
+        RandomSpawnPosition();
+    }
+
+    void RandomForce()
+    {
+        targetRB.AddForce(Vector3.up * Random.Range(minforce, maxforce), ForceMode.Impulse);
+    }
+   
+    void RandomTorque()
+    {
+        targetRB.AddTorque(Random.Range(minTorque, maxTorque), Random.Range(minTorque, maxTorque),
+            Random.Range(minTorque, maxTorque), ForceMode.Impulse);
+    }
+
+    void RandomSpawnPosition()
+    {
+        transform.position = new Vector3(Random.Range(minxpos, maxxpos), yspawnpos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
+
